972fe20144d955d6caa68a15fd784612e53d0b78
tpetiet <thibaultpetiet@gmail.com>
2021-01-06 15:09:13 +0100

    question 0.5

--- src/main/java/org/acme/graph/model/Edge.java
+++ src/main/java/org/acme/graph/model/Edge.java
@@ -1,5 +1,14 @@
 package org.acme.graph.model;
 
+import com.bedatadriven.jackson.datatype.jts.serialization.GeometrySerializer;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIdentityReference;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.LineString;
+
 /**
  * 
  * Un arc matérialisé par un sommet source et un sommet cible
@@ -37,7 +46,11 @@ public class Edge {
 	public void setId(String id) {
 		this.id = id;
 	}
-
+	@JsonIdentityInfo(
+			generator=ObjectIdGenerators.PropertyGenerator.class,
+			property="id")
+	
+	@JsonIdentityReference(alwaysAsId=true)
 	public Vertex getSource() {
 		return source;
 	}
@@ -59,5 +72,13 @@ public class Edge {
 	public String toString() {
 		return id + " (" + source + "->" + target + ")";
 	}
-
+	
+	@JsonSerialize(using = GeometrySerializer.class)
+    public LineString getGeometry() {
+        GeometryFactory gf = new GeometryFactory();
+        return (LineString)gf.createLineString(new Coordinate[] {
+            getSource().getCoordinate(),
+            getTarget().getCoordinate()
+        });
+    }
 }
